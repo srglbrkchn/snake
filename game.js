dd12ad203134dd9e460f0e859f73b35da5d3d1ea
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2023-10-18 23:46:39 +0200

    Start snake module. hardcode snake movement based on position of the head.

--- game.js
+++ game.js
@@ -1,1 +1,34 @@
-function main(curretnTiem) {}
+import {
+  draw as drawSnake,
+  update as updateSnake,
+  SNAKE_SPEED,
+} from "./snake.js";
+
+let lastRenderTime = 0;
+const gameBoard = document.getElementById("game-board");
+
+// game loop
+function main(currentTime) {
+  window.requestAnimationFrame(main);
+  const secSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secSinceLastRender < 1 / SNAKE_SPEED) {
+    return;
+  }
+
+  lastRenderTime = currentTime;
+
+  update();
+  draw();
+}
+
+window.requestAnimationFrame(main);
+
+function update() {
+  updateSnake();
+}
+
+function draw() {
+  // clear the view
+  gameBoard.innerHTML = "";
+  drawSnake(gameBoard);
+}
